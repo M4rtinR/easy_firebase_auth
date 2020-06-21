009a227c7841a978100c3a6dbf065b2b8431b565
Martin Ross <36231919+M4rtinR@users.noreply.github.com>
2020-06-21 17:30:47 +0100

    Update email_login_screen.dart
    Added print line so that I can see the value of variables.

--- lib/screens/email_login_screen.dart
+++ lib/screens/email_login_screen.dart
@@ -546,6 +546,7 @@ class _EmailLoginScreenState extends State<EmailLoginScreen> {
     }
 
     var _mainWidget = Container();
+    print('email: $_email password: $_password _isEmailRegistered: $_isEmailRegistered _isCorrectPassword: $_isCorrectPassword');
     if (_email == null || _password == null || !_isEmailRegistered || !_isCorrectPassword) {
       _mainWidget = _getEmailAndPasswordInput();
     } else if (_email == null && _isEmailRegistered == null) {
